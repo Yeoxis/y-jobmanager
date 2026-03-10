7c0d8812c04ad1e6e41b649da7893e20b9713bfa
Yeox <115135755+Yeoxis@users.noreply.github.com>
2026-03-10 05:57:23 -0400

    Update install.sql
    Hours now properly default to 0 instead of NULL after week reset

--- install.sql
+++ install.sql
@@ -13,8 +13,8 @@ CREATE TABLE IF NOT EXISTS `multijobs` (
 CREATE TABLE IF NOT EXISTS `y_timeclock` (
   `citizenid` varchar(100) NOT NULL,
   `job` varchar(50) NOT NULL,
-  `total_seconds` int(11) DEFAULT 0,
-  `weekly_seconds` int(11) DEFAULT 0,
+  `total_seconds` int(11) NOT NULL DEFAULT 0,
+  `weekly_seconds` int(11) NOT NULL DEFAULT 0,
   `week_start` date DEFAULT NULL,
   PRIMARY KEY (`citizenid`, `job`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
